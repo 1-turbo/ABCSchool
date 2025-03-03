386679a6de3c4aeefa516d6d6c723983cb5f466d
AElomda <elomdaa7444@gmail.com>
2025-03-03 17:08:53 +0200

    Added multi-tenancy service configuration using Finbuckle.MultiTenant.

---
+++ Infrastructure/Tenancy/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Finbuckle.MultiTenant;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Tenancy
+{
+    public static class Startup
+    {
+        public static IServiceCollection AddMultiTenancyServices(this IServiceCollection services, IConfiguration config)
+        {
+            return services
+                .AddDbContext<TenantDbContext>(options => options
+                .UseSqlServer(config.GetConnectionString("DefaultConnection")))
+                .AddMultiTenant<ABCSchoolTenantInfo>()
+                .WithHeaderStrategy("tenant")
+                .WithClaimStrategy("tenant")
+                .WithEFCoreStore<TenantDbContext, ABCSchoolTenantInfo>()
+                .Services;
+        }
+    }
+}
