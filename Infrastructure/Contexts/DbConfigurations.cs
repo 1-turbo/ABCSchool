6fbe94b0ec30bcd258fcd7623cafb5912db1ba29
AElomda <elomdaa7444@gmail.com>
2025-03-04 01:39:13 +0200

    Configure multi-tenant identity tables for users, roles, claims, and logins.

--- Infrastructure/Contexts/DbConfigurations.cs
+++ Infrastructure/Contexts/DbConfigurations.cs
@@ -1,5 +1,6 @@
 ﻿using Finbuckle.MultiTenant;
 using Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -25,6 +26,46 @@ namespace Infrastructure.Contexts
                     .ToTable("Roles", "Identity")
                     .IsMultiTenant();
             }
+        }
+
+        internal class ApplicationRoleClaimConfig : IEntityTypeConfiguration<ApplicationRoleClaim>
+        {
+            public void Configure(EntityTypeBuilder<ApplicationRoleClaim> builder) =>
+                builder
+                    .ToTable("RolesClaims", "Identity")
+                    .IsMultiTenant();
+        }
+
+        internal class ApplicationUserRoleConfig : IEntityTypeConfiguration<IdentityUserRole<string>>
+        {
+            public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder) =>
+                builder
+                    .ToTable("UserRoles", "Identity")
+                    .IsMultiTenant();
+        }
+
+        internal class ApplicationUserClaimConfig : IEntityTypeConfiguration<IdentityUserClaim<string>>
+        {
+            public void Configure(EntityTypeBuilder<IdentityUserClaim<string>> builder) =>
+                builder
+                    .ToTable("UserClaims", "Identity")
+                    .IsMultiTenant();
+        }
+
+        internal class ApplicationUserLoginConfig : IEntityTypeConfiguration<IdentityUserLogin<string>>
+        {
+            public void Configure(EntityTypeBuilder<IdentityUserLogin<string>> builder) =>
+                builder
+                    .ToTable("UserLogins", "Identity")
+                    .IsMultiTenant();
+        }
+
+        internal class ApplicationUserTokenConfig : IEntityTypeConfiguration<IdentityUserToken<string>>
+        {
+            public void Configure(EntityTypeBuilder<IdentityUserToken<string>> builder) =>
+                builder
+                    .ToTable("UserTokens", "Identity")
+                    .IsMultiTenant();
         }
     }
 }
