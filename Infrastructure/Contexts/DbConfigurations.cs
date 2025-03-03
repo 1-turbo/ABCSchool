1b9b754067ae647b7cebdebc7b57813f67b71307
AElomda <elomdaa7444@gmail.com>
2025-03-04 01:21:01 +0200

    Added multi-tenant configurations for ApplicationUser and ApplicationRole.

---
+++ Infrastructure/Contexts/DbConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using Finbuckle.MultiTenant;
+using Infrastructure.Identity.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Contexts
+{
+    internal class DbConfigurations
+    {
+        internal class ApplicationUserConfig : IEntityTypeConfiguration<ApplicationUser>
+        {
+            public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+            {
+                builder
+                    .ToTable("Users", "Identity")
+                    .IsMultiTenant();
+            }
+        }
+
+        internal class ApplicationRoleConfig : IEntityTypeConfiguration<ApplicationRole>
+        {
+            public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+            {
+                builder
+                    .ToTable("Roles", "Identity")
+                    .IsMultiTenant();
+            }
+        }
+    }
+}
