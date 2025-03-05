83221a689a753eb1d0c1d6320165fba62aa6b653
AElomda <elomdaa7444@gmail.com>
2025-03-05 03:08:57 +0200

    Added constants for school actions and features.

---
+++ Infrastructure/Constatnts/PermissionConstants.cs
@@ -0,0 +1,28 @@
+﻿namespace Infrastructure.Constatnts
+{
+    public static class SchoolAction
+    {
+        public const string Read = nameof(Read);
+        public const string Create = nameof(Create);
+        public const string Update = nameof(Update);
+        public const string Delete = nameof(Delete);
+        public const string UpgradeSubscription = nameof(UpgradeSubscription);
+    }
+
+    public static class SchoolFeature
+    {
+        public const string Tenants = nameof(Tenants);
+        public const string Users = nameof(Users);
+        public const string Roles = nameof(Roles);
+        public const string UserRoles = nameof(UserRoles);
+        public const string RoleClaims = nameof(RoleClaims);
+        public const string Schools = nameof(Schools);
+    }
+
+    public record SchoolPermission(string Action, string Feature, string Description, bool IsBasic = false, bool IsRoot = false)
+    {
+        public string Name => NameFor(Action, Feature);
+        public static string NameFor(string action, string feature) => $"Permission.{feature}.{action}";
+
+    }
+}
