2709a635b928ae068b103c898b9c0cc4077dd113
AElomda <elomdaa7444@gmail.com>
2025-03-14 23:20:12 +0200

    Added async failure response methods to return failed responses asynchronously.

--- Application/Wrappers/ResponseWrapper.cs
+++ Application/Wrappers/ResponseWrapper.cs
@@ -32,6 +32,21 @@
                 Messages = messages
             };
         }
+
+        public static Task<IResponseWrapper> FailAsync()
+        {
+            return Task.FromResult(Fail());
+        }
+
+        public static Task<IResponseWrapper> FailAsync(string message)
+        {
+            return Task.FromResult(Fail(message));
+        }
+
+        public static Task<IResponseWrapper> FailAsync(List<string> messages)
+        {
+            return Task.FromResult(Fail(messages));
+        }
         // Flag - Success
     }
 }
