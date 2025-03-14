95d1decad10a1953638b3fe563953d1069bfd6ad
AElomda <elomdaa7444@gmail.com>
2025-03-14 23:13:54 +0200

    Implemented IResponseWrapper for structured response handling. Added failure response methods with support for single and multiple messages.

---
+++ Application/Wrappers/ResponseWrapper.cs
@@ -0,0 +1,37 @@
+﻿namespace Application.Wrappers
+{
+    public class ResponseWrapper : IResponseWrapper
+    {
+        public List<string> Messages { get; set; } = [];
+        public bool IsSuccessful { get; set; }
+        public ResponseWrapper() { }
+
+        // Flag - Failure
+        public static IResponseWrapper Fail()
+        {
+            return new ResponseWrapper()
+            {
+                IsSuccessful = false
+            };
+        }
+
+        public static IResponseWrapper Fail(string message)
+        {
+            return new ResponseWrapper()
+            {
+                IsSuccessful = false,
+                Messages = [message]
+            };
+        }
+
+        public static IResponseWrapper Fail(List<string> messages)
+        {
+            return new ResponseWrapper()
+            {
+                IsSuccessful = false,
+                Messages = messages
+            };
+        }
+        // Flag - Success
+    }
+}
