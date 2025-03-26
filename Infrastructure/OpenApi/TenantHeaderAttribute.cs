ef1bb61db7c3864cb2a09711cc451dcfb7d66bbc
AElomda <elomdaa7444@gmail.com>
2025-03-27 00:15:06 +0200

    Add TenantHeaderAttribute for multi-tenant API support.

---
+++ Infrastructure/OpenApi/TenantHeaderAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Tenancy;
+
+namespace Infrastructure.OpenApi
+{
+    public class TenantHeaderAttribute()
+        : SwaggerHearderAttribute(
+            headerName: TenancyConstants.TenantIdName,
+            description: "Enter Your tenant name to access This Api.",
+            defaultName: string.Empty,
+            isRequired: true)
+    {
+    }
+}
