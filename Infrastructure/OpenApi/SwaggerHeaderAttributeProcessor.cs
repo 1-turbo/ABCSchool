826c0cd1345498ce66ac86664b14bc89083a6715
AElomda <elomdaa7444@gmail.com>
2025-04-03 15:46:10 +0200

    Implement global authentication processor for Swagger with JWT support.

---
+++ Infrastructure/OpenApi/SwaggerHeaderAttributeProcessor.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Schema;
+using NSwag;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using System.Reflection;
+
+namespace Infrastructure.OpenApi
+{
+    public class SwaggerHeaderAttributeProcessor : IOperationProcessor
+    {
+        public bool Process(OperationProcessorContext context)
+        {
+            if(context.MethodInfo.GetCustomAttributes(typeof(SwaggerHearderAttribute)) is SwaggerHearderAttribute swaggerHearder)
+            {
+                var parameters = context.OperationDescription.Operation.Parameters;
+
+                var existstingParam = parameters
+                    .FirstOrDefault(p => p.Kind == NSwag.OpenApiParameterKind.Header && p.Name == swaggerHearder.HeaderName);
+
+                if (existstingParam is not null)
+                {
+                    parameters.Remove(existstingParam);
+                }
+
+                parameters.Add(new OpenApiParameter
+                {
+                    Name = swaggerHearder.HeaderName,
+                    Kind = OpenApiParameterKind.Header,
+                    Description = swaggerHearder.Description,
+                    IsRequired = true,
+                });
+            }
+            return true;
+        }
+    }
+}
--- Infrastructure/OpenApi/SwaggerHearderAttribute.cs
+++ Infrastructure/OpenApi/SwaggerHearderAttribute.cs
@@ -1,11 +1,14 @@
-﻿namespace Infrastructure.OpenApi
+﻿using Newtonsoft.Json.Linq;
+
+namespace Infrastructure.OpenApi
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple =false)]
-    public class SwaggerHearderAttribute(string headerName, string description, string defaultName, bool isRequired) : Attribute
+    public class SwaggerHearderAttribute(string headerName, string description, string defaultName, bool isRequired , string defaultValue) : Attribute
     {
         public string HeaderName { get; set; } = headerName;
         public string Description { get; set; } = description;
         public string DefaultName { get; set; } = defaultName;
+        public string DefaultValue { get; set; } = defaultValue;
         public bool IsRequired { get; set; } = isRequired;
     }
 }
